3bb836f7ac4b593dfa1c6d027b2274087e3356bb
jeffrey-pc <847707695@qq.com>
2017-08-10 20:13:02 +0800

    'Updated'

--- hexo.py
+++ hexo.py
@@ -6,7 +6,7 @@ import os
 hexo_way = ur'E:\项目\blog'
 print 'cd ' + hexo_way
 os.system(ur'cd E:\项目\blog'.encode('gbk'))
-os.system('pwd')
+os.system('dir')
 os.system('hexo d \-g')
 os.system(ur'git add .')
 os.system("git commit -m 'Updated'")
